2b4ce2e41105a00743599dcc8b8c5ebc9aeac019
oleksiiKAV <43839148+oleksiiKAV@users.noreply.github.com>
2023-06-13 09:31:07 +0000

    Deploying to gh-pages from @ oleksiiKAV/goit-react-hw-07-phonebook@3a2522c05ab2e98d6b086a102d39a7c5880ec364 🚀

--- src/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as ContactForm } from './ContactForm/ContactForm';
-export { default as ContactList } from './ContactList/ContactList';
-export { default as Filter } from './Filter/Filter';
